4ac2a5e2c105e85249a525dd4d1684d2146bdd29
Илья Кулькин <ilakulkin@MacBook-Air-Ila.local>
2023-10-31 22:04:18 +0300

    feat: add docs

--- internal/controllers/transport_controller.go
+++ internal/controllers/transport_controller.go
@@ -15,10 +15,18 @@ type TransportController struct {
 }
 
 // Show
-// @Router			/api/Transport/{id} [get]
-// @Tags			TransportController
-// @Summary		Получение информации о транспорте по id
-// @Description	Получение информации о транспорте по id
+//
+//	@Router			/api/Transport/{id} [get]
+//	@Tags			TransportController
+//	@Summary		Получение информации о транспорте по id
+//	@Description	Получение информации о транспорте по id
+//
+//	@Param			id	path	int	true	"Id транспорта"
+//	@Accept			json
+//	@Produce		json
+//	@Success		200
+//	@Failure		404
+//	@Failure		500
 func (c TransportController) Show(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 
@@ -41,26 +49,32 @@ func (c TransportController) Show(w http.ResponseWriter, r *http.Request) {
 //
 //	@Router			/api/Transport [post]
 //	@Tags			TransportController
-//	@Summary		Получение информации о транспорте по id
-//	@Description	Получение информации о транспорте по id
+//	@Summary		Добавление нового транспорта
+//	@Description	Добавление нового транспорта
+//	@Param			body	body	dto.TransportDTO	true	"Параметры запроса"
+//	@Accept			json
+//	@Produce		json
+//	@Security		BearerAuth
 func (c TransportController) Store(w http.ResponseWriter, r *http.Request) {
 	log.Info(request.CreateTransportRequest(r))
 }
 
 // Edit
-// @Router			/api/Transport/{id} [put]
-// @Tags			TransportController
-// @Summary		Получение информации о транспорте по id
-// @Description	Получение информации о транспорте по id
+//
+//	@Router			/api/Transport/{id} [put]
+//	@Tags			TransportController
+//	@Summary		Изменение транспорта оп id
+//	@Description	Изменение транспорта оп id
 func (c TransportController) Edit(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // Destroy
-// @Router			/api/Transport/{id} [delete]
-// @Tags			TransportController
-// @Summary		Получение информации о транспорте по id
-// @Description	Получение информации о транспорте по id
+//
+//	@Router			/api/Transport/{id} [delete]
+//	@Tags			TransportController
+//	@Summary		Удаление транспорта по id
+//	@Description	Удаление транспорта по id
 func (c TransportController) Destroy(w http.ResponseWriter, r *http.Request) {
 
 	responders.NoContent(w)
